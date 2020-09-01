948de6ea1e5b8612114208f933b4f076e3ea4efe
Tommy <tomhoover1@gmail.com>
2020-09-01 12:36:42 -0400

    changed storage format
    phi/theta/magnitude/phase now written to csv as floats rather than ints

--- meas_ctrl/data/data_storage.py
+++ meas_ctrl/data/data_storage.py
@@ -2,7 +2,12 @@ def append_data(filename, data):
     file = open(filename, 'a')
     for i in range(0, len(data)):
         file.write('%s,%d,%f,%f,%f,%f\n' % (
-            data[i].measurement_type, data[i].freq, data[i].theta, data[i].phi, data[i].value_mag, data[i].value_phase))
+            data[i].measurement_type, 
+            data[i].freq, 
+            data[i].theta, 
+            data[i].phi, 
+            data[i].value_mag, 
+            data[i].value_phase))
     file.close()
 
 
