01a95ff1bace59dcb3da84fdab8b5a1bd8353457
Tommy <tomhoover1@gmail.com>
2020-09-01 12:35:06 -0400

    changed storage format
    phi/theta/magnitude/phase all stored as floats rather than ints now

--- meas_ctrl/data/data_storage.py
+++ meas_ctrl/data/data_storage.py
@@ -1,7 +1,7 @@
 def append_data(filename, data):
     file = open(filename, 'a')
     for i in range(0, len(data)):
-        file.write('%s,%d,%f,%f,%d,%d\n' % (
+        file.write('%s,%d,%f,%f,%f,%f\n' % (
             data[i].measurement_type, data[i].freq, data[i].theta, data[i].phi, data[i].value_mag, data[i].value_phase))
     file.close()
 
